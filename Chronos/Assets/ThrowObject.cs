7b23ba8510be96ce98fda8fbd0bfc39dd1a784be
Lanelore <bzankl@gmx.at>
2017-04-18 21:03:04 +0200

    Grab and throw items.

--- Chronos/Assets/ThrowObject.cs
+++ Chronos/Assets/ThrowObject.cs
@@ -6,22 +6,15 @@ public class ThrowObject : MonoBehaviour
     public Transform player;
     public Transform playerCam;
     public float throwForce = 10;
-    bool hasPlayer = false;
-    bool beingCarried = false;
-    public AudioClip[] soundToPlay;
-    // private AudioSource audio;
-    public int dmg;
-    private bool touched = false;
-
-    void Start()
-    {
-        //audio = GetComponent<AudioSource>();
-    }
+    public bool hasPlayer = false; // the item is within the player's reach
+    public bool beingCarried = false; // currently in the process of being picked up or carried around
+    public bool canThrow = false; // the player is no longer pressing the Action button, the pickup process has stopped or is finished
+    private bool touched = false; // the item touched a wall
 
     void Update()
     {
         float dist = Vector3.Distance(gameObject.transform.position, player.position);
-        if (dist <= 2.5f)
+        if (dist <= 2.5f && gameObject.GetComponent<Renderer>().isVisible)
         {
             hasPlayer = true;
         }
@@ -29,9 +22,20 @@ public class ThrowObject : MonoBehaviour
         {
             hasPlayer = false;
         }
-        if (hasPlayer && Input.GetButtonDown("Use"))
+        if (hasPlayer && Input.GetButtonDown("Action"))
         {
             GetComponent<Rigidbody>().isKinematic = true;
+            /*
+            float objectSize = gameObject.GetComponent<Renderer>().bounds.size.magnitude;
+            Vector3 hoverPosition = playerCam.transform.position + playerCam.transform.forward * objectSize;
+            float newDist = Vector3.Distance(hoverPosition, player.position);
+            if (newDist < dist)
+            {
+                return;
+                gameObject.transform.position = hoverPosition;
+            }
+            */
+            gameObject.transform.position = gameObject.transform.position + Vector3.up * 0.1f;
             transform.parent = playerCam;
             beingCarried = true;
         }
@@ -42,17 +46,22 @@ public class ThrowObject : MonoBehaviour
                 GetComponent<Rigidbody>().isKinematic = false;
                 transform.parent = null;
                 beingCarried = false;
+                canThrow = false;
                 touched = false;
             }
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetButtonUp("Action") && beingCarried)
             {
+                canThrow = true;
+            }
+            if (canThrow && beingCarried && Input.GetButtonDown("Action"))
+            {
+                canThrow = false;
                 GetComponent<Rigidbody>().isKinematic = false;
                 transform.parent = null;
                 beingCarried = false;
                 GetComponent<Rigidbody>().AddForce(playerCam.forward * throwForce);
-                // RandomAudio();
             }
-            else if (Input.GetMouseButtonDown(1))
+            else if (Input.GetMouseButtonDown(1)) //right click, 0 is left click
             {
                 GetComponent<Rigidbody>().isKinematic = false;
                 transform.parent = null;
@@ -60,21 +69,10 @@ public class ThrowObject : MonoBehaviour
             }
         }
     }
-    /*
-    void RandomAudio()
-    {
-        if (audio.isPlaying)
-        {
-            return;
-        }
-        audio.clip = soundToPlay[Random.Range(0, soundToPlay.Length)];
-        audio.Play();
 
-    }
-    */
-    void OnTriggerEnter()
+    void OnTriggerEnter(Collider other)
     {
-        if (beingCarried)
+        if (beingCarried && !other.CompareTag("Player"))
         {
             touched = true;
         }
