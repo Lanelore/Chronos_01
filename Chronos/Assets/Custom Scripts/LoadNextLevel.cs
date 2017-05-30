f9b652740b36dcda99a40e94fb9fc79c8192cb04
Lanelore <bzankl@gmx.at>
2017-05-30 21:34:33 +0200

    GoalParticle Prefab.

---
+++ Chronos/Assets/Custom Scripts/LoadNextLevel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadNextLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            int nextScene = SceneManager.GetActiveScene().buildIndex + 1;
+            if (nextScene < SceneManager.sceneCount)
+            {
+                SceneManager.LoadScene(nextScene);
+            }
+        }
+    }
+}
