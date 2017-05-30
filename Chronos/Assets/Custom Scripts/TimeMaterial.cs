614565ab7ee7841e9ac9c10978f39c01cf100ab5
Lanelore <bzankl@gmx.at>
2017-05-30 17:16:12 +0200

    Animate cube material and add more prefabs

---
+++ Chronos/Assets/Custom Scripts/TimeMaterial.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeMaterial : MonoBehaviour {
+
+    public float time = 1;
+    ManipulateTime mt;
+    public Material playMaterial;
+    public Material freezeMaterial;
+    public Material ffMaterial;
+
+    // Use this for initialization
+    void Start () {
+        mt = gameObject.GetComponent<ManipulateTime>();
+        if (mt == null)
+        {
+            mt = gameObject.transform.parent.gameObject.GetComponent<ManipulateTime>();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (time != mt.time)
+        {
+            time = mt.time;
+            UpdateMaterial();
+        }
+    }
+
+    void UpdateMaterial()
+    {
+        Renderer timeRenderer = gameObject.GetComponent<Renderer>();
+
+        if (time == 0)
+        {
+            timeRenderer.material = freezeMaterial;
+        }
+        else if (time == 1)
+        {
+            timeRenderer.material = playMaterial;
+        }
+        else if (time == 2)
+        {
+            timeRenderer.material = ffMaterial;
+        }
+    }
+}
