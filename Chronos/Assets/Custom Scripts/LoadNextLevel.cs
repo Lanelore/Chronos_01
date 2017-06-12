9b0545ad7a9ff72889842a55ee195c83000acec0
Lanelore <bzankl@gmx.at>
2017-06-12 23:13:54 +0200

    Lv2 floor first area.

--- Chronos/Assets/Custom Scripts/LoadNextLevel.cs
+++ Chronos/Assets/Custom Scripts/LoadNextLevel.cs
@@ -21,12 +21,9 @@ public class LoadNextLevel : MonoBehaviour {
 
     void OnTriggerEnter(Collider other)
     {
-        print("Trigger Door");
         if (other.gameObject.tag == "Player")
         {
-            print("Trigger Player");
             int nextScene = SceneManager.GetActiveScene().buildIndex + 1;
-            print("next " + nextScene + "; count " + SceneManager.sceneCount);
             if (nextScene <= SceneManager.sceneCount)
             {
                 SceneManager.LoadScene(nextScene);
